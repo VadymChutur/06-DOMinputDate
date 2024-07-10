643dcd5248678d50fdb6706ded4c1732431535a2
Vadym Chutur <falloutgreck@gmail.com>
2024-07-10 13:04:41 +0300

    upd

--- app/script.js
+++ app/script.js
@@ -7,12 +7,22 @@ function show() {
   const rangeEl = document.querySelector('.range-input');
   const dateEl = document.querySelector('.date-input');
   const colorEl = document.querySelector('.color-input');
-  const checkEl = document.querySelector('.check-input');
+  const checkEl = document.querySelector('#check-input');
+
+  const radioEl = document.querySelector('.radio[checked="checked"]');
   console.log(textEl.value);
   console.log(passEl.value);
   console.log(rangeEl.value);
   console.log(dateEl.value);
   console.log(colorEl.value);
-  console.log(checkEl.value);
+  console.log(checkEl.checked);
+  if (checkEl.checked) {
+    console.log('Yes');
+  } else {
+    console.log('No');
+  }
+
+  console.log(radioEl.value);
+
   console.log('uprove');
 }
